0116f3b700051a627f59e334a10ff2fe49fe86ae
sasalman29 <salman292md@gmail.com>
2026-08-06 01:43:11 +0600

    Add files via upload

---
+++ Chapter 1.js
@@ -0,0 +1,12 @@
+// Exercise 1
+console.log("Md Salman");
+console.log("Computer Science & Engineering");
+console.log("Metropolitan University");
+
+// Exercise 2
+// This program prints a learning journey statement.
+console.log("Learning JavaScript step-by-step for real-world projects!");
+
+// Exercise 3
+// console.log("Unclosed string error check); 
+// Output: SyntaxError: Invalid or unexpected token
