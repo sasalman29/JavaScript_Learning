32b19bd47a7c542ade166046a828acaff30f0078
sasalman29 <salman292md@gmail.com>
2026-08-07 04:50:40 +0600

    Add files via upload

---
+++ Chapter 02.js
@@ -0,0 +1,17 @@
+// Exercise 1
+const name = "MD Salman";
+const department = "Computer Science & Engineering";
+let semester = "10th Semester";
+console.log(`My name is ${name}, studying ${department} in the ${semester}.`);
+
+// Exercise 2
+console.log(typeof null); // "object"
+
+
+// Exercise 3
+const num = Number("19.5") + 0.5;
+console.log(num); // 20
+
+// Exercise 4
+console.log("5" + 3); // "53" (String concatenation)
+console.log("5" - 3); // 2 (Numeric subtraction via type coercion)
